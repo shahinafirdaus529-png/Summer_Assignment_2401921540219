94279bf28f4e0f2a4c1a5ce4a5e7a748b08f874b
shahinafirdaus529-png <shahinafirdaus529@gmail.com>
2026-06-02 21:14:05 +0530

    Update and rename README.md to MaximumSubarray.java

---
+++ Week_1_Arrays_Strings/Day_2_PrefixSum_SlidingWindow/MaximumSubarray.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public int maxSubArray(int[] nums) {
+
+        int currentSum = nums[0];
+        int bestSum = nums[0];
+
+        for(int i = 1; i < nums.length; i++) {
+
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+
+            if(currentSum > bestSum) {
+                bestSum = currentSum;
+            }
+        }
+
+        return bestSum;
+    }
+}
