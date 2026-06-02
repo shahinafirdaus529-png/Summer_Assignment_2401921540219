ceb4d129cdf5d1d6a9739db27509cdfa9cb56c99
shahinafirdaus529-png <shahinafirdaus529@gmail.com>
2026-06-02 21:16:36 +0530

    Add ContainsDuplicate class to check for duplicates

---
+++ Week_1_Arrays_Strings/Day_2_PrefixSum_SlidingWindow/ContainsDuplicate.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+
+    public boolean containsDuplicate(int[] values) {
+
+        HashSet<Integer> uniqueData = new HashSet<>();
+
+        for(int item : values) {
+
+            if(uniqueData.contains(item)) {
+                return true;
+            }
+
+            uniqueData.add(item);
+        }
+
+        return false;
+    }
+}
