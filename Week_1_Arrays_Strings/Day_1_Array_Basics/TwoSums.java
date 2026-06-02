3a8abf3513b5131c2ca93154694f7768fa685287
shahinafirdaus529-png <shahinafirdaus529@gmail.com>
2026-06-02 20:27:55 +0530

    Update TwoSums.java

--- Week_1_Arrays_Strings/Day_1_Array_Basics/TwoSums.java
+++ Week_1_Arrays_Strings/Day_1_Array_Basics/TwoSums.java
@@ -1,1 +1,17 @@
+import java.util.*;
+
+ class Solution {
+     public int[] twoSum(int[] numbers, int targetValue) {
+         HashMap<Integer, Integer> valueMap = new HashMap<>();
+  for(int currentIndex = 0; currentIndex < numbers.length; currentIndex++) { 
+    int requiredValue = targetValue - numbers[currentIndex];
+     if(valueMap.containsKey(requiredValue)) { 
+        return new int[]{ valueMap.get(requiredValue), currentIndex }; 
+        }
+         valueMap.put(numbers[currentIndex], currentIndex);
+          }
+           return new int[]{};
+            } 
+            }
+
 
