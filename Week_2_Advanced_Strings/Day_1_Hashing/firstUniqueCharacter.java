e063579ff6667b1a235966e646fb7b4a45df7ae8
shahinafirdaus529-png <shahinafirdaus529@gmail.com>
2026-06-08 16:15:00 +0530

    Implement first unique character finder in Java

---
+++ Week_2_Advanced_Strings/Day_1_Hashing/firstUniqueCharacter.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public int firstUniqChar(String s) {
+
+        int[] frequency = new int[26];
+
+        for(char ch : s.toCharArray()) {
+
+            frequency[ch - 'a']++;
+        }
+
+        for(int i = 0; i < s.length(); i++) {
+
+            if(frequency[s.charAt(i) - 'a'] == 1) {
+
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
