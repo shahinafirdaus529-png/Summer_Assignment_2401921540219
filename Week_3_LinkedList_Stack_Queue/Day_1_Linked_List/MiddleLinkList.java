e8c1a6c6357c5e9eaff07a5df5d046c5c60d26fc
shahinafirdaus529-png <shahinafirdaus529@gmail.com>
2026-06-16 14:58:28 +0530

    Create MiddleLinkList.java

---
+++ Week_3_LinkedList_Stack_Queue/Day_1_Linked_List/MiddleLinkList.java
@@ -0,0 +1,27 @@
+public class Solution {
+
+    public ListNode middleNode(
+        ListNode head
+    ) {
+
+        ListNode slow =
+            head;
+
+        ListNode fast =
+            head;
+
+        while(
+            fast != null &&
+            fast.next != null
+        ) {
+
+            slow =
+                slow.next;
+
+            fast =
+                fast.next.next;
+        }
+
+        return slow;
+    }
+}
